aa4c65ef53bf554337f6a28bb2a9eb734441f8da
mahamadou-simpara <msimpara@doninya.com>
2023-10-19 23:18:38 +0000

    date formatted

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,13 +2,20 @@ import './ExpenseItem.css'
 
 function ExpenseItem(props) {
 
-  
+    const month = props.date.toLocaleString('en-US', { month: 'long' });
+    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+    const year = props.date.getFullYear();
+
 
     return (
     <div className='expense-item'>
-        <div className='expense-item__date'>{props.date.toDateString()}</div>
+        <div className='expense-item__date'>
+            <div>{month}</div>
+            <div>{year}</div>
+            <div>{day}</div>
+        </div>
         <div className='expense-item__description'>
-            <h2>{props.titlt}</h2>
+            <h2>{props.title}</h2>
             <div className='expense-item__price'>${props.amount}</div>
         </div>
 
